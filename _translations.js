8147b8b80fd22e87bd20cf47c99337724bb6ab01
Wilson Gouanet <wilstito@gmail.com>
2022-03-17 04:38:39 -0400

    Add the files coming from main app

---
+++ _translations.js
@@ -0,0 +1,16 @@
+function hnd_ut(a) {
+  a.TRANSLATIONS['Search term too short'] = 'Terme recherché est trop court';
+  a.TRANSLATIONS['No results'] = 'Aucun résultat';
+  a.TRANSLATIONS['Please enter 3 or more characters'] =
+    'Entrez 3 caractères ou plus';
+  a.TRANSLATIONS[
+    'Word list not ready yet. Please wait until the word list is fully downloaded'
+  ] =
+    'La liste de mot est momentanément indisponible. Veuillez attendre le tchargement complet de la liste';
+  a.TRANSLATIONS[
+    'Incorrect or corrupt search data. Please check your HelpNDoc template'
+  ] = ' Donnée recherchée incorrecte ou corrompue';
+  a.TRANSLATIONS['Related topics...'] = 'Articles relatifs...';
+  a.TRANSLATIONS['Loading...'] = 'Chargement...';
+  a.TRANSLATIONS['Close'] = 'Fermer';
+}
